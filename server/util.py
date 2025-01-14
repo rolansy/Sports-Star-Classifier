2363cd6e9c5a50494da4f23592b453a3b53132ad
rolansy <ronalshoeygeorge@gmail.com>
2025-01-14 14:03:15 +0530

    Implement image classification and model loading functions

--- server/util.py
+++ server/util.py
@@ -5,8 +5,52 @@ import base64
 import cv2
 from wavelet import w2d
 
+__class_name_to_number={}
+__class_number_to_name={}
+__model=None
+
 def classify_image(image_base64_data,file_path=None):
-    pass
+    imgs=get_cropped_image_if_2_eyes(file_path,image_base64_data)
+    result=[]
+
+    for img in imgs:
+        scalled_raw_img = cv2.resize(img, (32, 32))
+        img_har = w2d(img, 'db1', 5)
+        scalled_img_har = cv2.resize(img_har, (32, 32))
+        combined_img = np.vstack((scalled_raw_img.reshape(32 * 32 * 3, 1), scalled_img_har.reshape(32 * 32, 1)))
+
+        len_image_array = 32 * 32 * 3 + 32 * 32
+
+        final=combined_img.reshape(1,len_image_array).astype(float)
+
+        result.append({
+            'class':class_number_to_name(__model.predict(final)[0]),
+            'class_probability':np.round(__model.predict_proba(final)*100,2).tolist()[0],
+            'class_dictionary':__class_name_to_number
+        })
+
+
+
+    return result
+
+
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+    global __class_name_to_number
+    global __class_number_to_name
+
+    with open("./artifacts/class_dictionary.json", "r") as f:
+        __class_name_to_number = json.load(f)
+        __class_number_to_name = {v:k for k,v in __class_name_to_number.items()}
+
+    global __model
+    if __model is None:
+        with open('./artifacts/saved_model.pkl', 'rb') as f:
+            __model = joblib.load(f)
+    print("loading saved artifacts...done")
+
+def class_number_to_name(class_number):
+    return __class_number_to_name[class_number]
 
 def get_cv2_image_from_base64_string(b64str):
     encoded_data = b64str.split(',')[1]
@@ -42,4 +86,5 @@ def get_64_virat():
 
 
 if __name__ == '__main__':
+    load_saved_artifacts()
     print(classify_image((get_64_virat()),None))
